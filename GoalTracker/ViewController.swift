1d0b4fd99e9cc1c963e8ae70af3344838ac74c54
Stephen Doyle <stephendoyle75@gmail.com>
2015-01-15 22:03:41 +0000

    Initial Commit

---
+++ GoalTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalTracker
+//
+//  Created by Stephen Doyle on 15/01/2015.
+//  Copyright (c) 2015 Stephen Doyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
